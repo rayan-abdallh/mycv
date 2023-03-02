fbae97c6b79fe937fbb946527d760795e480aab4
ahmed555morgan <77603122+ahmed555morgan@users.noreply.github.com>
2023-03-02 18:55:28 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
+    <!-- Navbar -->
+    <div class="navbar">
+      <div class="container flex">
+        <img class="logo" alt="Vue logo" src="logo2.jpeg">
+        
+        <nav>
+          <ul>
+          
+            
+         
+            <li><a href="#home">HOME</a></li>
+            <li><a href="#create cv">CREATE-CV</a></li>
+            <li><a href="#bautus">BAUTUS</a></li>
+            <li><a href="#login">LOGIN</a></li>
+            <li><a href="#en">EN🌍</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    
+ 
+
+
   </nav>
   <router-view/>
+
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.showcase-form{
+  height: 558px;
+width: 402px;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
